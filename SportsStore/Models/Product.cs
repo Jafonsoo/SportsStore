c0327f20914ebc98e64c7371b591f969dda2c8fc
João Aires <48353968+joaoaires@users.noreply.github.com>
2020-10-30 09:53:27 +0000

    Adicionar arquivos de projeto.

---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+       
+        [StringLength(256)]
+        public string Name { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+
+
+
+
+
+    }
+}
